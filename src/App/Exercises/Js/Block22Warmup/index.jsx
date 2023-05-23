3a0c4bbf865e3cbc9c4007ed7012d28d31473ef9
Konrad Bonicki <konradbonicki29@gmail.com>
2023-05-23 21:09:00 +0200

    block22

--- src/App/Exercises/Js/Block22Warmup/index.jsx
+++
@@ -1,5 +0,0 @@
-import './styles.css';
-
-export function Block22Warmup() {
-  return <div>Block22Warmup</div>;
-}
---
+++ src/App/Exercises/Js/SetTimeout/router-data.jsx
@@ -0,0 +1,10 @@
+import { SetTimeout } from './index';
+
+export const blockRouterMetaData = {
+  path: 'set-timeout-and-set-interval',
+  date: '23-05-2023',
+  linkLabel: 'Funkcje wywoływane czasowo',
+  blockNo: 22,
+  element: <SetTimeout />,
+  tags: ['setInterval', 'setTimeout', 'time'],
+};
