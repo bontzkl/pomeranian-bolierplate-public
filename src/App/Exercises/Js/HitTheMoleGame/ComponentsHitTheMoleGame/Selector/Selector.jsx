4398c6cf145bf915e8ab272f67996dedba5f471b
Konrad Bonicki <konradbonicki29@gmail.com>
2023-05-26 19:32:40 +0200

    game menu

--- src/App/Exercises/Js/HitTheMoleGame/ComponentsHitTheMoleGame/Selector/Selector.jsx
+++ src/App/Exercises/Js/HitTheMoleGame/ComponentsHitTheMoleGame/Selector/Selector.jsx
@@ -1,14 +1,27 @@
-import { useState } from 'react';
 import { Button } from '../Button/Button';
+import { useState } from 'react';
+export const Selector = ({ options }) => {
+  const [modifiedOptions, setModifiedOptions] = useState(options);
 
-export const Selector = (options) => {
-  const [time, setTime] = useState(2);
-  const [moles, setMoles] = useState(1);
-
+  const handleClick = (clickedButton) => {
+    setModifiedOptions(
+      modifiedOptions.map((option) => {
+        return {
+          ...option,
+          isActive: clickedButton === option.value,
+        };
+      })
+    );
+  };
   return (
     <div>
-      {options.map(({ label, value }) => (
-        <Button isActive={moles === value} onClick={() => setMoles(value)}>
+      {modifiedOptions.map(({ label, value, isActive }) => (
+        <Button
+          isActive={isActive}
+          onClick={() => {
+            handleClick(value);
+          }}
+        >
           {label}
         </Button>
       ))}
